783719f1ef033f7b3c9fb65691872f811d8843a6
righteous <51360743+lzy755277@users.noreply.github.com>
2025-11-15 23:47:41 +0800

    Resize window from 360x900 to 550x900

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(360, 900);
+  Win32Window::Size size(550, 900);
   if (!window.Create(L"celechron", origin, size)) {
     return EXIT_FAILURE;
   }
